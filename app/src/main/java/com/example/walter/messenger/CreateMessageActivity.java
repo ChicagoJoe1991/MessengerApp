5ae16cb3cae0c51cc4d6c6ff0448a8747d16e7ce
ChicagoJoe1991 <dabears1.1.1@gmail.com>
2016-02-17 11:30:09 -0600

    added a message to our create activity intent, referenced the EditText view and created a string from the text within it.

--- app/src/main/java/com/example/walter/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/example/walter/messenger/CreateMessageActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.widget.EditText;
 
 
 public class CreateMessageActivity extends AppCompatActivity {
@@ -23,7 +24,15 @@ public class CreateMessageActivity extends AppCompatActivity {
 
     //call onSendMessage() when the button is clicked
     public void onSendMessage(View view){
+        //get a reference to the EditText view
+        EditText messageView = (EditText) findViewById(R.id.message);
+        // get the text from the EditText view, allocate memory for it and make it a string
+        String messageText = messageView.getText().toString();
+        //Create a new Intent
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        //add extra information to the intent, in this case the string from the EditText view
+        intent.putExtra("message",messageText);
+        //send the activity to android for review
         startActivity(intent);
 
     }

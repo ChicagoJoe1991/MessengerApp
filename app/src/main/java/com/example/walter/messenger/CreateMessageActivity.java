cd32e33c7dfaddf96d1b63fd52dc23ce3ac2479a
ChicagoJoe1991 <dabears1.1.1@gmail.com>
2016-02-17 14:09:26 -0600

    add and implement createChooser method: forces user to choose which activity they would like to start without the option to select a default

--- app/src/main/java/com/example/walter/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/example/walter/messenger/CreateMessageActivity.java
@@ -29,14 +29,20 @@ public class CreateMessageActivity extends AppCompatActivity {
         EditText messageView = (EditText) findViewById(R.id.message);
         // get the text from the EditText view, allocate memory for it and make it a string
         String messageText = messageView.getText().toString();
-        //Create a new Intent
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        //Create a new Intent to send an implicit intent to activities that have the
+        //ACTION_SEND action in their intent filters
+        Intent intent = new Intent(Intent.ACTION_SEND);
         /*add extra information to the intent, in this case the string from the EditText view,
         Also, ReceiveMessageActivity.EXTRA_MESSAGE is a constant for the name of the exxtra
         information. We will add it to ReceiveMessageActivity next.*/
-        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
+        intent.putExtra(Intent.EXTRA_TEXT,messageText);
+        //get a reference to the chooser title, the getString() method is used to get the value
+        //of a string resource
+        String chooserTitle = getString(R.string.chooser);
+        //display the chooser dialog
+        Intent chosenIntent = Intent.createChooser(intent,chooserTitle);
         //send the activity to android for review
-        startActivity(intent);
+        startActivity(chosenIntent);
 
     }
 
